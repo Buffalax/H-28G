8bb3e49f40b5ae973f475ed0875644a3f648f480
Marin Marinov <gcnew@abv.bg>
2014-04-09 15:15:14 +0300

    - refactoring  - ring spawning moved to render()

--- H-28G.js
+++ H-28G.js
@@ -1,75 +1,116 @@
-var rings = [];
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext('2d');
-var WIDTH = canvas.width;
-var HEIGHT = canvas.height;
-var CENTER_X = WIDTH / 2;
-var CENTER_Y = HEIGHT / 2;
-var MAX_SIDE = Math.max(WIDTH, HEIGHT);
-var MAX_LINE_WIDTH = 20;
-var RING_SPAWN_RATE = 1000;
-var FPS = 60;
+/*global window, document*/
+/*jshint laxbreak:true*/
 
-var INITIAL_SPEED = 4;
-var SPEED = INITIAL_SPEED;
-var SPEED_INCREMENT = 0;
+var Util = (function() {
+	return {
+		oncef: function(aFunction) {
+			var started = false;
 
-spawnRing = function () {
-    rings.unshift(new Ring());
-};
+			return function() {
+				if (!started) {
+					started = true;
+					aFunction.apply(this, arguments);
+				}
+			};
+		}
+	};
+})();
 
-action = function () {
-    for (var i = 0, len = rings.length; i < len; i++) {
-        rings[i].act();
-    }
-};
+function Game() {
+	var rings = [];
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext('2d');
+	var WIDTH = canvas.width;
+	var HEIGHT = canvas.height;
+	var CENTER_X = WIDTH / 2;
+	var CENTER_Y = HEIGHT / 2;
+	var MAX_SIDE = Math.max(WIDTH, HEIGHT);
+	var MAX_LINE_WIDTH = 20;
+	var RING_SPAWN_RATE = 1000;
 
-drawBgr = function () {
-    context.rect(0, 0, WIDTH, HEIGHT);
-    context.fillStyle = '#FFFFFF';
-    context.fill();
-};
+	var INITIAL_SPEED = 4;
+	var SPEED = INITIAL_SPEED;
+	var SPEED_INCREMENT = 0;
 
-draw = function () {
-    requestAnimationFrame(function () {
-        drawBgr();
-        for (var i = 0, len = rings.length; i < len; i++) {
-            rings[i].draw();
-        }
-    });
-};
+	var ticker = (function() {
+		return window.requestAnimationFrame
+			|| window.webkitRequestAnimationFrame
+			|| window.mozRequestAnimationFrame
+			|| window.msRequestAnimationFrame
+			|| window.oRequestAnimationFrame
+			|| function(callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+	})();
 
-loop = function () {
-    action();
-    draw();
-    SPEED += SPEED_INCREMENT;
-};
+	function spawnRing() {
+		rings.unshift(new Ring());
+	}
 
-function Ring() {
-    this.radius = 10;
-    this.draw = function () {
-        emptyRing(this.radius);
-    };
-    this.act = function () {
-        if (this.radius > MAX_SIDE) {
-            rings.pop();
-        }
-        this.radius += SPEED * (this.radius / MAX_SIDE);
-    };
+	function action() {
+		for (var i = 0, len = rings.length; i < len; i++) {
+			rings[i].act();
+		}
+	}
+
+	function drawBgr() {
+		context.rect(0, 0, WIDTH, HEIGHT);
+		context.fillStyle = '#FFFFFF';
+		context.fill();
+	}
+
+	function draw() {
+		drawBgr();
+		for (var i = 0, len = rings.length; i < len; i++) {
+			rings[i].draw();
+		}
+	}
+
+	function emptyRing(radius) {
+		context.beginPath();
+		context.arc(CENTER_X, CENTER_Y, radius, 0, 2 * Math.PI, false);
+		context.lineWidth = MAX_LINE_WIDTH * (radius / MAX_SIDE);
+		context.strokeStyle = '#000000';
+		context.stroke();
+	}
+
+	function Ring() {
+		this.radius = 10;
+
+		this.draw = function () {
+			emptyRing(this.radius);
+		};
+
+		this.act = function () {
+			if (this.radius > MAX_SIDE) {
+				rings.pop();
+			}
+			this.radius += SPEED * (this.radius / MAX_SIDE);
+		};
+	}
+
+	var lastSpawn = 0;
+	function render() {
+		var now = +Date.now();
+
+		if ((lastSpawn + RING_SPAWN_RATE) < now) {
+			spawnRing();
+			lastSpawn = lastSpawn ? (lastSpawn + RING_SPAWN_RATE) : now;
+		}
+
+		action();
+		draw();
+		SPEED += SPEED_INCREMENT;
+	}
+
+	function loop() {
+		ticker(loop);
+		render();
+	}
+
+	return {
+		start: Util.oncef(loop)
+	};
 }
 
-emptyRing = function (radius) {
-    context.beginPath();
-    context.arc(CENTER_X, CENTER_Y, radius, 0, 2 * Math.PI, false);
-    context.lineWidth = MAX_LINE_WIDTH * (radius / MAX_SIDE);
-    context.strokeStyle = '#000000';
-    context.stroke();
-};
-
-
-initGame = function () {
-    setInterval(spawnRing, RING_SPAWN_RATE);
-    setInterval(loop, 1000 / FPS);
-};
-
-initGame();
+new Game().start();
